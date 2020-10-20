2885894f88ab37a56a7d5ebce5ec325fc32767dd
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2020-10-20 13:02:06 +0300

    Сделана сеть для варианта 6 и сформирован отчет

---
+++ set_ip.sh
@@ -0,0 +1,6 @@
+ip r add 10.0.10.0/24 via 172.16.0.20 dev nk_tap_ubuntu
+ip r add 10.0.20.0/24 via 172.16.0.20 dev nk_tap_ubuntu
+ip r add 10.0.30.0/24 via 172.16.0.20 dev nk_tap_ubuntu
+ip r add 10.0.40.0/24 via 172.16.0.20 dev nk_tap_ubuntu
+ip r add 10.0.50.0/24 via 172.16.0.20 dev nk_tap_ubuntu
+ip r add 10.0.60.0/24 via 172.16.0.20 dev nk_tap_ubuntu
